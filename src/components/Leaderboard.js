89d8b5e1c1d4620f50960d65964e6c99b657f4f6
morjep <94778792+morjep@users.noreply.github.com>
2022-07-13 12:22:57 +0200

    added table in Leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,4 +1,34 @@
+import { useSelector } from "react-redux";
+import { userStatsArraySorted } from "../app/appSlice";
+
 export const Leaderboard = () => {
-  return <div>Leaderboard</div>;
+  const usersStats = useSelector(userStatsArraySorted);
+
+  console.log("usersStats: ", usersStats);
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Questions</th>
+            <th>Answers</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersStats.map((user) => (
+            <tr key={user.user}>
+              <td>{user.user}</td>
+              <td>{user.questions}</td>
+              <td>{user.answers}</td>
+              <td>{user.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default Leaderboard;
